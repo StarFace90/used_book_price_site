6b12b0e29f14ee2bb6a7807f1fc50c7ee0a6405e
woody-park <starface1009@gmail.com>
2022-02-01 23:53:10 +0900

    hook연습 및 서버와 hello world 연결

--- used-books/src/App.js
+++ used-books/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+
+
+// 기본적인 서버와의 연결을 위한 테스트 컴포넌트 없이 일단 app에서 테스트 후 분리
 function App() {
+
+  const [data, setData] = useState({ data: [] });
+
+  // 서버에서 데이터 받지 않으면 loading 되게끔..
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+
+    const apiRequest = async () => {
+      const axiosRequest = await axios(
+        'http://localhost:5000/'
+      );
+      console.log(axiosRequest)
+      setData(axiosRequest.data);
+      setLoading(false)
+    }
+    apiRequest();
+  }, []);
+
+  // 서버에서 데이터 받지 않으면 loading 되게끔..
+  if (loading) return <div>Loading...</div>
+  console.log(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {/* {data.map(item => (
+        <li key={item.id}><h1>{item.id}</h1>
+          <h1>{item.name}</h1>
+        </li>
+      ))} */}
+      {data}
+      {/* hello world */}
+
+    </>
+  )
 }
 
 export default App;
